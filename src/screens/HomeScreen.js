2c6973bb1e0c6c8502132a7b9582769bd9d0a15b
Bhavesh Varu <bhavesh.varu97@gmail.com>
2024-01-29 13:37:12 +0530

    updated homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { listProduct } from '../actions/productActions'
